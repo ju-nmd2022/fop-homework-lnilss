6aaeb658ba5c551e4a8ab60730c50a059afb9d50
lnilss <117316458+lnilss@users.noreply.github.com>
2023-01-16 20:11:04 +0100

    Update emoji.js
    Updated with comments

--- emoji.js
+++ emoji.js
@@ -1,36 +1,38 @@
-// My first emoji
+/*
+My first emoji created in JavaScript
+Based on the pleading face emoji
+*/
 
 background(255, 255, 255);
 
+// Emoji face
 noStroke();
 fill(230, 210, 50);
 ellipse(150, 150, 200);
 
+//Emoji eyes
 noStroke();
 fill(0, 0, 0);
 ellipse(115, 140, 60);
 
+fill(0, 0, 0);
+ellipse(185, 140, 60);
+
+//Emoji pupils
 noStroke();
 fill(255, 255, 255);
 ellipse(105, 135, 35);
 
-noStroke();
 fill(255, 255, 255);
 ellipse(130, 150, 15);
 
-noStroke();
-fill(0, 0, 0);
-ellipse(185, 140, 60);
-
-noStroke();
 fill(255, 255, 255);
 ellipse(175, 135, 35);
 
-noStroke();
 fill(255, 255, 255);
 ellipse(200, 150, 15);
 
-noStroke();
+//Emoji mouth
 fill(0, 0, 0);
 ellipse(150, 200, 30);
 
