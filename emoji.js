47d30bd5acdbf7e31edc6f41b01288fdb9847be2
lnilss <117316458+lnilss@users.noreply.github.com>
2023-01-16 19:51:52 +0100

    Add files via upload

---
+++ emoji.js
@@ -0,0 +1,39 @@
+// My first emoji
+
+background(255, 255, 255);
+
+noStroke();
+fill(230, 210, 50);
+ellipse(150, 150, 200);
+
+noStroke();
+fill(0, 0, 0);
+ellipse(115, 140, 60);
+
+noStroke();
+fill(255, 255, 255);
+ellipse(105, 135, 35);
+
+noStroke();
+fill(255, 255, 255);
+ellipse(130, 150, 15);
+
+noStroke();
+fill(0, 0, 0);
+ellipse(185, 140, 60);
+
+noStroke();
+fill(255, 255, 255);
+ellipse(175, 135, 35);
+
+noStroke();
+fill(255, 255, 255);
+ellipse(200, 150, 15);
+
+noStroke();
+fill(0, 0, 0);
+ellipse(150, 200, 30);
+
+noStroke();
+fill(230, 210, 50);
+ellipse(150, 180, 40);
